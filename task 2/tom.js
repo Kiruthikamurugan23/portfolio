eac10ad6b541f20ebc819d662b174424ec37bfe2
Kiruthikamurugan23 <97123532+Kiruthikamurugan23@users.noreply.github.com>
2024-01-23 22:59:38 +0530

    Update tom.js

--- task 2/tom.js
+++ task 2/tom.js
@@ -1,2 +1,15 @@
-document.getElementById('myforms').addEventListener('submit',(event)=>{
+document.getElementById('myform').addEventListener('submit', (event) => {
     event.preventDefault();
+
+    var firstname = document.getElementById("fname").value;
+    var lastname = document.getElementById("lname").value;
+    var emailid = document.getElementById("emailid").value;
+    var mobileno = document.getElementById("mobileno").value;
+    var city = document.getElementById("city").value;
+
+    document.getElementById("fname-r8").innerHTML = firstname;
+    document.getElementById("lname-r8").innerHTML = lastname;
+    document.getElementById("email-r8").innerHTML = emailid;
+    document.getElementById("mobile-r8").innerHTML = mobileno;
+    document.getElementById("city-r8").innerHTML = city;
+});
